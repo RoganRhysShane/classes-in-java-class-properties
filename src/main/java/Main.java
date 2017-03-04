6f86e3ad498b5120c9839d5f23bf074dc189dd55
RoganRhysShane <chrisoliver08@live.com>
2017-03-04 11:38:33 -0500

    properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,20 +9,32 @@ public class Main {
     public static void main(String[] args) {
 
         // todo: create a new variable "planet1" and set it to a new instance of the Planet class
-
+        Planet planet1 = new Planet();
         // todo: set all four properties of the Planet class on the planet1 instance: name, distanceFromSun, satellites, and supportsLife.
+        planet1.name = "Mercury";
+        planet1.distanceFromSun = 50.;
+        planet1.satellites = 4;
+        planet1.supportsLife = false;
 
         // todo: create another new variable named "planet2" and set it to a new instance of the Planet class
+        Planet planet2 = new Planet();
+
 
         // todo: set all four properties of the Planet class on the planet2 instance
+        planet2.name = "Jupiter";
+        planet2.distanceFromSun = 5000.;
+        planet2.satellites = 420;
+        planet2.supportsLife = true;
 
         // todo: Use println() to print a String description of planet1 using string concatenation
         /*
             Example output: "Name: Earth, Distance from Sun: 1 AU, Satellites: 1 moon(s), Supports Life: true"
          */
-
+        System.out.println("Name: " + planet1.name + ", Distance from Sun: " + planet1.distanceFromSun
+            + ", Satellites: " + planet1.satellites + " moon(s), Supports Life: " + planet1.supportsLife);
         // todo: Use println() to print a String description of planet2, just like above
-
+        System.out.println("Name: " + planet2.name + ", Distance from Sun: " + planet2.distanceFromSun
+                + ", Satellites: " + planet2.satellites + " moon(s), Supports Life: " + planet2.supportsLife);
         /*
             Next, we're going to demonstrate that properties of an object can be
             changed. This change impacts only that one property on that one
@@ -30,9 +42,9 @@ public class Main {
          */
 
         // todo: change a single property on your planet1 instance
-
+        planet1.satellites = 42;
         // todo: change a different property on your planet2 instance
-
+        planet2.name = "Neptune";
         // todo: Use println() to print a String description of planet1 again.
         /*
             Note that the property you changed on planet1 is reflected in the
@@ -40,9 +52,11 @@ public class Main {
             shows that these two instances are distinct and separate from each
             other.
          */
-
+        System.out.println("Name: " + planet1.name + ", Distance from Sun: " + planet1.distanceFromSun
+                + ", Satellites: " + planet1.satellites + " moon(s), Supports Life: " + planet1.supportsLife);
         // todo: Use println() to print a String description of planet2, again.  Note that the property you changed on planet2 is reflected in the output.
-
+        System.out.println("Name: " + planet2.name + ", Distance from Sun: " + planet2.distanceFromSun
+                + ", Satellites: " + planet2.satellites + " moon(s), Supports Life: " + planet2.supportsLife);
     }
 
 }
--- src/main/java/Planet.java
+++ src/main/java/Planet.java
@@ -6,7 +6,7 @@
 public class Planet {
 
     // todo: create a property named "name" that can hold a planet's name. EG: mercury
-
+    String name;
     /*
         A planet's distance from the sun is measured in Astronomical Units (AU).
         One AU is the distance from the Sun to Earth.
@@ -15,8 +15,13 @@ public class Planet {
      */
     // todo: create a property named "distanceFromSun" that holds a planet's distance from the sun in Astronomical Units.
 
+    double distanceFromSun;
+
     // todo: create a property "satellites" that holds the number of moons a planet has. EG: Mercury has 0 satellites
 
-    // todo: create a property "supportsLife" that indicates whether or not a planet supports life. EG: Mercury does not support life.
+    int satellites;
 
+
+    // todo: create a property "supportsLife" that indicates whether or not a planet supports life. EG: Mercury does not support life.
+    boolean supportsLife;
 }
